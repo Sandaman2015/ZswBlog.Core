d63ef36648eabb2fb66ef51a677818ab5553de5c
sandaman2015@163.com <sandaman2015@163.com>
2020-09-06 23:08:57 +0800

    更新第三方音乐站组件

---
+++ ZswBlog.ThirdParty/Music/MusicHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZswBlog.Util;
+using ZswBlog.DTO;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace ZswBlog.ThirdParty.Music
+{
+    public class MusicHelper
+    {
+        public static List<MusicDTO> GetMusicListByCount(int count) {
+            string url = ConfigHelper.GetValue("musicSite");
+            string jsonResult = RequestHelper.HttpGet(url, Encoding.UTF8);
+            MusicList musicList = JsonConvert.DeserializeObject<MusicList>(jsonResult);
+
+            List<MusicDTO> musicDTOs = new List<MusicDTO>();
+            List<MusicTracks> musicTracks= musicList.playlist.trackIds;
+            //遍历歌单列表
+            foreach (MusicTracks tracks in musicTracks)
+            {
+                count--;
+                //获取歌曲详情
+                var songsData =string.Format("http://121.36.93.244:3000/song/detail?ids={0}",tracks.id);
+                string dataResult = RequestHelper.HttpGet(songsData, Encoding.UTF8);
+                MusicSongs musicSongs = JsonConvert.DeserializeObject<MusicSongs>(dataResult);
+
+                //获取歌曲歌词
+                var songsLyric = string.Format("http://121.36.93.244:3000/lyric?id={0}", tracks.id);
+                string lyricResult = RequestHelper.HttpGet(songsLyric, Encoding.UTF8);
+                Musiclyric musiclyric = JsonConvert.DeserializeObject<Musiclyric>(lyricResult);
+
+                //获取歌曲连接
+                var songsUrl= string.Format("http://121.36.93.244:3000/song/url?id={0}", tracks.id);
+                string songsUrlResult = RequestHelper.HttpGet(songsUrl, Encoding.UTF8);
+                MusicUrlData musicUrl = JsonConvert.DeserializeObject<MusicUrlData>(songsUrlResult);
+
+                //填充歌曲歌词
+                string lyric = null;
+
+                //判断是有歌词和不为纯音乐
+                if (!musiclyric.nolyric && !musiclyric.uncollected) {
+                    lyric = musiclyric.lrc.lyric;
+                }
+                List<string> nameList= musicSongs.songs[0].ar.Select(a => a.name).ToList();                
+                musicDTOs.Add(new MusicDTO()
+                {
+                    title = musicSongs.songs[0].name,
+                    artist = string.Join(",", nameList),
+                    pic=musicSongs.songs[0].al.picUrl,                    
+                    lrc= lyric,
+                    src= musicUrl.data[0].url
+                });
+                if (count == 0) {
+                    break;
+                }
+            }
+            return musicDTOs;
+        }
+    }
+    
+}
--- ZswBlog.Util/RequestHelper.cs
+++ ZswBlog.Util/RequestHelper.cs
@@ -1,9 +1,13 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Text;
 
 namespace ZswBlog.Util
 {
+    /// <summary>
+    /// 简易HttpHelper类
+    /// </summary>
     public class RequestHelper
     {
         public static string HttpGet(string url, Encoding encoding)
@@ -21,5 +25,76 @@ namespace ZswBlog.Util
             return json;
 
         }
+        public static string HttpGet(string url, Dictionary<string, string> dic)
+        {
+            string param = GetParam(dic);
+            string getUrl = string.Format("{0}?{1}", url, param);
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(getUrl);
+            req.Method = "GET";
+            req.ContentType = "application/x-www-form-urlencoded";
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            string result = "";
+            using (StreamReader reader = new StreamReader(resp.GetResponseStream(), Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+
+        public static Stream HttpGet(string url)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "Get";
+            request.ContentType = "application/json;charset=UTF-8";
+            request.UserAgent = null;
+            request.Timeout = 5000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            return response.GetResponseStream();
+            //Stream myResponseStream = response.GetResponseStream();
+            //StreamReader myStreamReader = new StreamReader(myResponseStream);
+            //string json = myStreamReader.ReadToEnd();
+            //return json;
+
+        }
+
+
+        public static string HttpPost(string url, Dictionary<string, string> dic)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            string param = GetParam(dic);
+            byte[] data = Encoding.UTF8.GetBytes(param);
+            req.ContentLength = data.Length;
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+            }
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            string result = "";
+            using (StreamReader reader = new StreamReader(resp.GetResponseStream(), Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+
+       
+
+        private static string GetParam(Dictionary<string, string> dic)
+        {
+            StringBuilder builder = new StringBuilder();
+            int i = 0;
+            foreach (var item in dic)
+            {
+                if (i > 0)
+                    builder.Append("&");
+                builder.AppendFormat("{0}={1}", item.Key, item.Value);
+                i++;
+            }
+            return builder.ToString();
+        }
     }
+
 }
